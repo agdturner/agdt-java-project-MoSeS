bcb21aa1bc9b64c932890ded0d5a20ae4e04816d
geoagdt <geoagdt@GEO-L10624>
2017-10-24 23:44:55 +0100

    Name simplification and got rid of static methods.

--- src/main/java/uk/ac/leeds/ccg/andyt/projects/moses/io/ParameterFileParser.java
+++ src/main/java/uk/ac/leeds/ccg/andyt/projects/moses/io/ParameterFileParser.java
@@ -2,19 +2,19 @@
  * A component of a library for
  * <a href="http://www.geog.leeds.ac.uk/people/a.turner/projects/MoSeS">MoSeS</a>.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.
  */
 package uk.ac.leeds.ccg.andyt.projects.moses.io;
 
@@ -22,91 +22,81 @@ import uk.ac.leeds.ccg.andyt.generic.core.Generic_ErrorAndExceptionHandler;
 import uk.ac.leeds.ccg.andyt.generic.io.Generic_StaticIO;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.StreamTokenizer;
 
 /**
  * Class for parsing Parameter Files.
+ *
  * @author geoagdt
  */
 public class ParameterFileParser {
 
-	public static Object[] parse(File aFile) {
-		Object[] tInput_Parameters = new Object[12];
-		BufferedReader aBufferedReader = null;
-		try {
-			aBufferedReader = 
-                    new BufferedReader(
-                    new InputStreamReader(
-					new FileInputStream(aFile)));
-		} catch (FileNotFoundException _FileNotFoundException) {
-			System.err.println(_FileNotFoundException.getLocalizedMessage());
-            System.exit(Generic_ErrorAndExceptionHandler.FileNotFoundException);
-		}
-		StreamTokenizer aStreamTokenizer = new StreamTokenizer(aBufferedReader);
-		Generic_StaticIO.setStreamTokenizerSyntax5(aStreamTokenizer);
-		String _String;
-		int index = 0;
-		try {
-			int tokenType = aStreamTokenizer.nextToken();
-			while (tokenType != StreamTokenizer.TT_EOF) {
-				switch (tokenType) {
-				case StreamTokenizer.TT_EOL:
-					index++;
-					break;
-				case StreamTokenizer.TT_WORD:
-					_String = aStreamTokenizer.sval;
-					switch (index) {
-					case 0:
-						tInput_Parameters[index] = new File(_String);
-						break;
-					case 1:
-						tInput_Parameters[index] = _String;
-						break;
-					case 2:
-						tInput_Parameters[index] = _String;
-						break;
-					case 3:
-						tInput_Parameters[index] = new Integer(_String);
-						break;
-					case 4:
-						tInput_Parameters[index] = new Integer(_String);
-						break;
-					case 5:
-						tInput_Parameters[index] = new Integer(_String);
-						break;
-					case 6:
-						tInput_Parameters[index] = new Integer(_String);
-						break;
-					case 7:
-						tInput_Parameters[index] = new Integer(_String);
-						break;
-					case 8:
-						tInput_Parameters[index] = new Integer(_String);
-						break;
-					case 9:
-						tInput_Parameters[index] = new Long(_String);
-						break;
-					case 10:
-						tInput_Parameters[index] = _String;
-						break;
-					case 11:
-						tInput_Parameters[index] = _String;
-						break;
-					}
-					// System.out.println(_String);
-					break;
-				}
-				tokenType = aStreamTokenizer.nextToken();
-			}
-            aBufferedReader.close();
-		} catch (IOException aIOException) {
-			System.err.println(aIOException.getLocalizedMessage());
+    public static Object[] parse(File f) {
+        Object[] result = new Object[12];
+        BufferedReader br;
+        br = Generic_StaticIO.getBufferedReader(f);
+        StreamTokenizer st = new StreamTokenizer(br);
+        Generic_StaticIO.setStreamTokenizerSyntax5(st);
+        String s;
+        int index = 0;
+        try {
+            int tokenType = st.nextToken();
+            while (tokenType != StreamTokenizer.TT_EOF) {
+                switch (tokenType) {
+                    case StreamTokenizer.TT_EOL:
+                        index++;
+                        break;
+                    case StreamTokenizer.TT_WORD:
+                        s = st.sval;
+                        switch (index) {
+                            case 0:
+                                result[index] = new File(s);
+                                break;
+                            case 1:
+                                result[index] = s;
+                                break;
+                            case 2:
+                                result[index] = s;
+                                break;
+                            case 3:
+                                result[index] = new Integer(s);
+                                break;
+                            case 4:
+                                result[index] = new Integer(s);
+                                break;
+                            case 5:
+                                result[index] = new Integer(s);
+                                break;
+                            case 6:
+                                result[index] = new Integer(s);
+                                break;
+                            case 7:
+                                result[index] = new Integer(s);
+                                break;
+                            case 8:
+                                result[index] = new Integer(s);
+                                break;
+                            case 9:
+                                result[index] = new Long(s);
+                                break;
+                            case 10:
+                                result[index] = s;
+                                break;
+                            case 11:
+                                result[index] = s;
+                                break;
+                        }
+                        // System.out.println(_String);
+                        break;
+                }
+                tokenType = st.nextToken();
+            }
+            br.close();
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
             System.exit(Generic_ErrorAndExceptionHandler.IOException);
-		}
-		return tInput_Parameters;
-	}
+        }
+        return result;
+    }
 }
